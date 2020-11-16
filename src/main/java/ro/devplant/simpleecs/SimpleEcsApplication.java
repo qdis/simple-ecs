7168fdf8aca1d59dc2d192d69f2ad182f4ff3649
Timo Bejan <timo@devplant.ro>
2020-11-16 11:53:08 +0200

    first commit

---
+++ src/main/java/ro/devplant/simpleecs/SimpleEcsApplication.java
@@ -0,0 +1,13 @@
+package ro.devplant.simpleecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleEcsApplication.class, args);
+    }
+
+}
